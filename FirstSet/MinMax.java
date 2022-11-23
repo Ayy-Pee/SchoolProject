c50ee03c54140f62c85a3a44b89c2b1d1b4bafb4
ArjunP <115101452+Ayy-Pee@users.noreply.github.com>
2022-11-23 20:50:20 +0530

    Update MinMax.java

--- FirstSet/MinMax.java
+++ FirstSet/MinMax.java
@@ -4,11 +4,11 @@ class MinMax
     public static void main(String[]args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value or x");
+        System.out.println("Enter value of x");
         int x = sc.nextInt();
-        System.out.println("Enter value or y");
+        System.out.println("Enter value of y");
         int y = sc.nextInt();
-        System.out.println("Enter value or z");
+        System.out.println("Enter value of z");
         int z = sc.nextInt();
         System.out.println("Maximum number is: "+Math.max(x,Math.max(y,z)));
         System.out.println("Minimum number is: "+Math.min(x,Math.min(y,z)));
